73ba6fbc25314507343c89ac75dbc9f56b677424
Pedro Maximino <pedro.maximino@sinfo.org>
2021-12-28 23:10:14 +0000

    Basic implementation of ServerApp.

--- server/src/main/java/sirs/remotedocs/ServerApp.java
+++ server/src/main/java/sirs/remotedocs/ServerApp.java
@@ -1,8 +1,33 @@
-package java.sirs.remotedocs;
+package sirs.remotedocs;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
 
 public class ServerApp {
 	public static void main(String[] args) {
+
 		System.out.println(ServerApp.class.getSimpleName());
+
+		try {
+			if (args.length != 1) {
+				System.err.println("Invalid arguments");
+				System.err.printf("Usage: java %s <port>", ServerApp.class.getSimpleName());
+				System.exit(-1);
+			}
+
+			int port = Integer.parseInt(args[0]);
+			ServerServiceImpl serverService = new ServerServiceImpl();
+			Server server = ServerBuilder
+					.forPort(port)
+					.addService(serverService)
+					.build()
+					.start();
+			server.awaitTermination();
+		} catch (InterruptedException | IOException e) {
+			System.err.println("Something went wrong: " + e.getMessage());
+		}
 		
 	}
 }
