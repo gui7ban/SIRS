6a78747fa8f0ec63f150f94de5b61ec1d7ef7e3d
Pedro Maximino <pedro.maximino@sinfo.org>
2022-01-11 16:20:20 +0000

    Added table for file permissions and added ownerId to files table.

--- schema/schema.sql
+++ schema/schema.sql
@@ -8,7 +8,21 @@ CREATE TABLE IF NOT EXISTS remotedocs_users
 
 CREATE TABLE IF NOT EXISTS remotedocs_files
 (
-    id   SERIAL NOT NULL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    digest VARCHAR(5000) NOT NULL
+    id      SERIAL NOT NULL PRIMARY KEY,
+    name    VARCHAR(100) NOT NULL,
+    digest  VARCHAR(5000) NOT NULL,
+    ownerId INTEGER NOT NULL,
+    FOREIGN KEY (ownerId)
+        REFERENCES remotedocs_users(ownerId) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS remotedocs_permissions
+(
+    userId     INTEGER NOT NULL,
+    fileId     INTEGER NOT NULL,
+    permission INTEGER NOT NULL,
+    FOREIGN KEY (userId)
+        REFERENCES remotedocs_users(userId) ON DELETE CASCADE
+    FOREIGN KEY (fileId)
+        REFERENCES remotedocs_files(fileId) ON DELETE CASCADE
 );
