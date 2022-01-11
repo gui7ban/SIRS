85d52111697389dc2f13222e36b45797d4bd30c6
Pedro Maximino <pedro.maximino@sinfo.org>
2022-01-11 16:06:16 +0000

    Added ServerRepo to Server.

--- server/src/main/java/sirs/remotedocs/domain/Server.java
+++ server/src/main/java/sirs/remotedocs/domain/Server.java
@@ -1,8 +1,12 @@
 package sirs.remotedocs.domain;
 
-import sirs.remotedocs.domain.exception.ErrorMessage;
+import sirs.remotedocs.Logger;
+import sirs.remotedocs.ServerRepo;
 import sirs.remotedocs.domain.exception.RemoteDocsException;
 public class Server {
+
+	private ServerRepo serverRepo = new ServerRepo();
+	private Logger logger = new Logger("Server", "Core");
 
 	public void login (String name, String password) throws RemoteDocsException {
 	
