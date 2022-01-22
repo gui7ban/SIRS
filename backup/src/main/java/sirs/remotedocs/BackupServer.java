f5ef081d86b2a0206700b713888a2620d1ae09d3
Pedro Maximino <pedro.maximino@sinfo.org>
2022-01-22 15:56:59 +0000

    Implemented method to send backup server public key upon request.

--- backup/src/main/java/sirs/remotedocs/BackupServer.java
+++ backup/src/main/java/sirs/remotedocs/BackupServer.java
@@ -1,4 +1,28 @@
 package sirs.remotedocs;
 
+import sirs.remotedocs.crypto.AsymmetricCryptoOperations;
+import sirs.remotedocs.exceptions.BackupServerException;
+
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+
 public class BackupServer {
+
+    private KeyPair keyPair;
+
+    public BackupServer() {
+        try {
+            this.keyPair = AsymmetricCryptoOperations.generateKeyPair();
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Failed to generate key pair for backup server.");
+        }
+    }
+
+    public PublicKey getPublicKey() throws BackupServerException {
+        if (keyPair == null)
+            throw new BackupServerException("No public key available at this time.");
+
+        return this.keyPair.getPublic();
+    }
 }
--- backup/src/main/java/sirs/remotedocs/BackupServerService.java
+++ backup/src/main/java/sirs/remotedocs/BackupServerService.java
@@ -1,9 +1,32 @@
 package sirs.remotedocs;
 
+import com.google.protobuf.ByteString;
+import io.grpc.stub.StreamObserver;
+import sirs.remotedocs.backupgrpc.Backupcontract.*;
 import sirs.remotedocs.backupgrpc.RemoteDocsBackupGrpc;
+import sirs.remotedocs.exceptions.BackupServerException;
+
+import java.security.PublicKey;
+
+import static io.grpc.Status.INVALID_ARGUMENT;
 
 public class BackupServerService extends RemoteDocsBackupGrpc.RemoteDocsBackupImplBase {
 
     private final BackupServer backupServer = new BackupServer();
 
+    @Override
+    public void getPublicKey(PublicKeyRequest request, StreamObserver<PublicKeyResponse> responseObserver) {
+        try {
+            PublicKey publicKey = this.backupServer.getPublicKey();
+            PublicKeyResponse response = PublicKeyResponse
+                    .newBuilder()
+                    .setKey(ByteString.copyFrom(publicKey.getEncoded()))
+                    .build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } catch (BackupServerException e) {
+            responseObserver.onError(INVALID_ARGUMENT.withDescription(e.getMessage()).asRuntimeException());
+        }
+    }
 }
