b5e2ce5ff1da3f4c8a594a1d6a91c7380f65d145
Pedro Maximino <pedro.maximino@sinfo.org>
2022-01-23 18:17:41 +0000

    Added channelEnd method to server frontend.

--- server/src/main/java/sirs/remotedocs/ServerFrontend.java
+++ server/src/main/java/sirs/remotedocs/ServerFrontend.java
@@ -9,7 +9,7 @@ import sirs.remotedocs.backupgrpc.Backupcontract.PublicKeyResponse;
 import sirs.remotedocs.backupgrpc.RemoteDocsBackupGrpc;
 
 public class ServerFrontend {
-    private RemoteDocsBackupGrpc.RemoteDocsBackupBlockingStub stub;
+    private final RemoteDocsBackupGrpc.RemoteDocsBackupBlockingStub stub;
     private final ManagedChannel channel;
 
     public ServerFrontend(String path) {
@@ -25,4 +25,8 @@ public class ServerFrontend {
     public EncryptedResponse handshake(EncryptedRequest request) { return this.stub.handshake(request); }
 
     public EncryptedResponse backupServerFiles(EncryptedRequest request) { return this.stub.backupServerFiles(request); }
+
+    public void channelEnd() {
+        channel.shutdownNow();
+    }
 }
