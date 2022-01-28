2ecacb2c91fec1a0b63ee7e0239d0c8f5e9dee9b
Pedro Maximino <pedro.maximino@sinfo.org>
2022-01-28 21:54:59 +0000

    Works.

--- client/src/test/java/ClientTests.java
+++ client/src/test/java/ClientTests.java
@@ -1,7 +1,15 @@
-package sirs.remotedocs.demo;
-
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
 import sirs.remotedocs.ClientApp;
+import sirs.remotedocs.LoginRegisterForm;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ClientTests {
-    ClientApp clientApp = new ClientApp("localhost", 8080);
+    private ClientApp clientApp = new ClientApp("localhost", 8080);
+
+    @BeforeAll
+    void setUp() {
+        this.clientApp.switchForm(this.clientApp.getMenu(), this.clientApp.getRegister());
+    }
+
 }
